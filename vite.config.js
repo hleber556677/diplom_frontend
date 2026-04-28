1a6bed07f8472ca29b5575700b3e1083ade7df3b
hleber556677 <sergeevg22@gmail.com>
2026-04-28 16:17:22 +0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === "true" ? "/diplom_api/" : "/",
+  base: "/diplom_frontend/",
   plugins: [react()],
   server: {
     proxy: {
